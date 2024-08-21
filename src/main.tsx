43f6af7780ee70256e3405ec1f2a214d3442926b
dunde <chldlsrb1000@naver.com>
2024-08-21 15:05:00 +0900

    update: separate path logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routesObject } from "./pages/routes.tsx";
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  }
-])
+const router = createBrowserRouter(routesObject)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/pages/routes.tsx
@@ -0,0 +1,11 @@
+import { RouteObject } from "react-router-dom";
+import App from "../App.tsx";
+
+const routesObject: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />,
+  }
+]
+
+export { routesObject }
