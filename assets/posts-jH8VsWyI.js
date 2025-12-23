783ad811cb5d8f18ebf11174b8835a83e4c48a50
inkyu.choi <chldlsrb1000@naver.com>
2025-12-23 09:02:38 +0000

    Updates

---
+++ assets/posts-jH8VsWyI.js
@@ -0,0 +1,189 @@
+const n=[{id:"2d1ca0f9-b5ae-80ab-ad48-e2218c646286",title:"[ADP] 1. 데이터 이해 - 데이터의 이해",slug:"2d1ca0f9b5ae80abad48e2218c646286",createdAt:"2025-12-22",updatedAt:"2025-12-23",category:"자격증",tags:["공부","ADP"],coverImage:"https://prod-files-secure.s3.us-west-2.amazonaws.com/a1836829-6459-497a-a170-7dc7c046b453/ba276813-f0e7-4776-9a08-23a824347ba3/adp.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB4665CLM2UJF%2F20251223%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20251223T090227Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEEaCXVzLXdlc3QtMiJGMEQCIElua7mDF%2FFipRNcarqb9H47EIG9ADRKbad9owtfvLX2AiBYT0MYWQiYrLmi55S2FsN5MgvC6Y68izT7IKzB5Ode5Cr%2FAwgKEAAaDDYzNzQyMzE4MzgwNSIMypb4%2BN3cGc9ig8VoKtwDRL3Cf8ZO7smkYUBL1sHBE94GalP%2FNGC1iYsAEwEzkV2BNF8HxtGUsB9neZo85q7Rc%2FzttaFwXCMOLlLmoz2XkXs4V3M34FEOfPE4nh%2BsRBKlf5aa1EXi2J2j5l%2BRkXbi%2BGbmkRqeCVhPb1VgRzZWyDf612JCpIsXu9bQrB%2F11BBy0W3w4Jr5miq9gjDzx4wEcqQAjfKwdyi%2Fsiz%2FkOruoZ05AQ0DUdlQYT5Jfd0TaedP8u%2BwIgqSckwK4msPlwJsEQKH8d0WMd4z91WVTB0Pgw01jUHI4hX%2BLsnuLj5Hdel73vrjEHZLLJBQR%2FsEHpTdDBfilFnm6CVTB2f93F3LzLOgyKFk%2B8mxbL1Y09OROg2kfj61PUrgEVFEDjnz7gdhuRNAQ8HaA0yyIdwXYAKFEpGP%2BVKkGg9vyQChpmw7cNRbTcFmiytIxfGH9SoQiJmc%2B7Tki8DFixcr%2BDAl9DfToYA8QsXretO%2F9F%2F7vM9KDuHtq%2F8loNzpFfct3yte2Q9znv5%2BS0mwX13PBgMSQX50FF51E%2FmoNeA%2FdMTLQBO6hhSrZ5bG5yGM%2B8%2BfjIHBiga50Nus4wt%2FMR3lbTu1MA0Ul5jz1O9Racjjq56vUqkwidtkKo1oXbya0yHfLPYw1LOpygY6pgFZRyfX3nHwVALHhLcQVxyWOboLCJR8a8gcfc5Glob%2Bbtxid3hLv4B3G52XorX4VuWI6HunLc%2FQe0X%2FpkBq0jLMYr%2BSorzpz7gxZ55j59GQKUmiXp1jW6dyPoh7PYiHePM6NdC0oDpMueU6Ijw72ir7AV9i9g87Q0Zy%2BJxSEad2LnadHw9%2Fz%2FS9lSGUhh58YCGS93X%2FyJISOo93VDi5HEKi0qOxFbHs&X-Amz-Signature=29c8074e9609d012964642da69d870b72467b8a6d165c36d0d35dc7b2f020b79&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",content:`# 데이터와 정보
+
+## 데이터 정의
+추론과 추정의 근거를 이루는 사실, 다른 객체와 상호 관계 속에 가치를 가짐.
+
+## 데이터 특성
+- 존재적 특성: 객관적 사실(Fact, Raw Material)
+- 당위적 특성: 추론, 예측, 전망, 추정을 위한 근거(Basis)
+
+## 데이터 유형
+- 정성적 데이터(Qualitative Data): 비정형, 주관적, 저장/검색/분석에 많은 비용 소모
+- 정량적 데이터(Quantitative Data): 정형, 객관적, 적은 비용 소모
+> 차이점 확인
+
+## 지식경영 핵심 이슈
+- 암묵지: 경험 지식, 공유 어려움, 개인에게 축적된 내면화(Internalization) 지식 → 조직의 지식으로 공통화(Socialization)
+- 형식지: 문서 지식, 공유 용이, 숫자나 언어로 표출화(Externalization) 지식 → 개인의 지식으로 연결과(Combination)
+
+## 데이터와 정보의 관계
+
+### DIKW의 정의
+- 데이터(Data): 객관적 사실, 개별로 의미 중요 X
+- 정보(Information): 데이터 가공, 처리 후 의미 O
+- 지식(Knowledge): 정보 구조화, 개인 경험 결합으로 고유의 지식으로 내재화
+- 지혜(Wisdom): 지식 축적 및 아이디어 결합된 창의적 산물
+
+### DIKW 피라미드
+데이터, 정보, 지식을 통해 지혜를 얻는 계층구조 설명
+
+
+\`\`\`text
+예시) 피라미드에서는 하단부터 데이터 → 지혜 순서
+- A 마켓의 펜 100원, B 마켓 펜 200원 → 데이터
+- A 마켓의 펜이 더 쌈 → 정보
+- 더 싼 A 마켓의 펜을 사려함 → 지식
+- A 마켓의 다른 문구류도 B 마켓보다 쌀거라 생각 → 지혜
+\`\`\`
+
+
+> 정의와 예시 확인
+
+# 데이터베이스의 정의와 특징 
+
+## 용어의 연혁
+- **1950년대** : 미국 군대 군비 상황 관리를 위해 컴퓨터 도서관을 설립. 데이터 기지 → Data Base
+- 1963년 6월 : SDC가 데이터베이스 용어 공식사용
+- 1963년 : GE의 C.바크만이 DBMS인 IDS 개발
+- 1965년 : 데이터베이스 시스템 용어 등장
+- 1970년대 초반 : 유럽에서 데이터베이스 용어 일반화
+- **1975년** : 미국 CAC가 KORSTIC을 통해 서비스 → 우리나라에서 데이터베이스 사용
+- 1980년 : KORSTIC이 TECHNOLINE 이라는 온라인 정보 검색 서비스 개시 → 본격적인 데이터베이스 서비스 시대
+- **1980년대 중반** : 국내 데이터베이스 관련 기술 연구 개발
+
+## 데이터베이스의 정의
+
+### 1차 개념 (정형 데이터)
+
+
+|<center>출처</center>|<center>내용</center>|
+|---|---|
+|<center>EU<br><데이터베이스의 법적 보호에 관한 지침></center>|체계적이거나 조직적으로 정리되고 전자식 또는 기타 수단으로 개별적으로 접근할 수 있는 독립된 저작물, 데이터 또는 기타 소재의 수집물|
+|<center>국내<br>‘저작권법’</center>|소재를 체계적으로 배열 또는 구성한 편집물로서 개별적으로 그 소재에 접근하거나 그 소재를 검색할 수 있도록 한 것|
+
+### 2차 개념(비정형 데이터 → 빅데이터)
+
+
+|<center>출처</center>|<center>내용</center>|
+|---|---|
+|<center>국내<br>’컴퓨터 용어사전’</center>|동시에 복수의 적용 업무를 지원할 수 있도록 복수 이용자의 요구에 대응해서 데이터를 받아들이고 저장, 공급하기 위하여 일정한 구조에 따라서 편성된 데이터의 집합|
+|<center>국내<br>’Wikipedia’</center>|관련된 레코드의 집합, 소프트웨어로는 데이터베이스 관리 시스템(DBMS)을 의미|
+|<center>국내<br>’데이터분석 전문가 가이드’</center>|문자, 기호, 음성, 화상, 영상 등 상호 관련된 다수의 콘텐츠를 정보 처리 및 정보통신 기기에 의하여 체계적으로 수집, 축적하여 다양한 용도와 방법으로 이용할 수 있도록 정리한 정보의 집합체|
+
+
+> 데이터베이스 정의와 흐름만 파악
+
+## 데이터베이스의 특징
+
+### 데이터베이스의 일반적인 특징
+
+|<center>데이터베이스 특징</center>|<center>설명</center>|
+|---|---|
+|<center>통합된 데이터<br>Integrated Data</center>|중복 X, 중복은 관리상의 복잡한 부작용 초래|
+|<center>저장된 데이터<br>Stored Data</center>|컴퓨터 접근용 저장 매체에 저장, DB 는 기본 컴퓨터 기술 바탕|
+|<center>공용 데이터<br>Shared Data</center>|여러 사용자 공동 이용, 대용량 및 복잡한 구조|
+|<center>변화되는 데이터<br>Changeable Data</center>|저장 내용은 곧 현시점 상태. 단, 삽입, 삭제, 갱신으로 변화되도 현재의 정확한 데이터 유지|
+
+> 자주 출제됨. 특징 맞거나 틀린 것 선택
+
+### 데이터베이스의 다양한 측면에서의 특징
+- **정보의 축적 및 전달**
+  - 기계 가독성 : 일정한 형식 → 컴퓨터(정보처리기기) 읽고 쓰기 가능
+  - 검색 가독성 : 다양한 방법 → 검색 가능
+  - 원격 조작성 : 원거리에서 즉시 이용
+- 정보 이용 : 원하는 다양한 정보를 신속 정확하고 경제적으로 찾아냄.
+- 정보 관리 : 질서, 구조에 따라 정리, 저장, 검색, 관리 → 축적, 추가, 갱신 용이
+- 정보기술 발전 : 정보처리, 소프트웨어, 하드웨어, 네트워크 등 발전
+- 경제/산업 : 인프라 특성으로 경제, 산업, 사회 활동 효율 → 국민 편의 증진 수단
+
+# 데이터베이스의 활용
+
+## 기업 내부 데이터베이스
+정보통신망 구축 가속화 → 1990년대 기업 내부 데이터베이스가 전사 시스템으로 확대
+
+### 1980년대
+- OLTP(On-Line Transaction Processing): 여러 단말에서 요청 → 호스트가 DB 엑세스 → 처리결과 리턴
+- OLAP(On-Line Analytical Processing): 정보 분석, 비즈니스 관점으로 데이터 접근 및 의사 결정에 도움되는 정보 획득 기술.
+> 약어의 의미 확인, OLTP → 데이터 갱신, OLAP → 데이터 조회
+
+|<center>구분</center>|<center>OLTP</center>|<center>OLAP</center>|
+|---|---|---|
+|<center>구조</center>|<center>복잡</center>|<center>단순</center>|
+|<center>갱신</center>|<center>동적 → 순간적</center>|<center>정적 → 주기적</center>|
+|<center>응답 시간</center>|<center>수 초 내</center>|<center>초 ~ 분 사이</center>|
+|<center>범위</center>|<center>수 십일</center>|<center>오랜 기간</center>|
+|<center>성격</center>|<center>정규적 핵심 데이터</center>|<center>비정규적 읽기 전용 데이터</center>|
+|<center>크기</center>|<center>수 GB</center>|<center>수 TB</center>|
+|<center>내용</center>|<center>현재 데이터</center>|<center>요약 데이터</center>|
+|<center>특성</center>|<center>트랜잭션 중심</center>|<center>주제 중심</center>|
+|<center>엑세스 빈도</center>|<center>높음</center>|<center>보통</center>|
+|<center>질의 결과 예측</center>|<center>주기적, 예측 가능</center>|<center>예측 힘듬</center>|
+
+> ADP 과정에는 비교 부분 출제 가능성. 참조 → 소설처럼 읽는 DB 모델링 이야기(영진닷컴)
+
+### 2000년대
+- **CRM**(Customer Relationship Management) : 고객관계관리, 기업이 고객 관련 내외부 자료 분석통합 → 고객 중심 자원 극대화 → 고객 특성 마케팅 활동을 계획, 지원 평가
+- **SCM**(Supply Chain Management) : 공급망관리, 기업에서 공급망 단계 최적화 → 수요자의 제품을 원하는 시간과 장소에 제공
+
+### 각 분야별 내부 데이터베이스
+
+#### 1. 분야별 데이터 베이스 개념
+- 제조
+  - 2000년 기점 전환, 클라이언트/서버 내부 정보시스템 → 웹기반 DB 전환 중
+  - 대기업 위주 ERP → CRM 발전, 중소기업 투자 확대 필요성 인지. RTE를 통한 협업적 IT 비중 확대
+- 금융
+  - 1998년 IMF 이후 확산
+  - 2000년대 초, EAI, ERP, e-CRM으로 정보 공유 및 통합 → 고객 정보 전략적 활용
+  - 2000년대 중, DW(Data Warehouse) 도입 → DB활용 마케팅 강화, 최적화를 위한 BI 기반 시스템 구축
+  - 바젤2 같은 대형 프로젝트 마무리 이후 EDW(Enterprise Data Warehouse)확장이 DB 시장 확장에 기여
+- 유통
+  - 2000년 이후, IT 환경 변화에 따라 CRM, SCM 구축 활발
+  - 상거래 인프라 및 KMS 백업시스템 구축 진행
+  - RFID 등장 → 유비쿼터스 시대 준비
+
+#### 2. 분야별 데이터 베이스 소개
+- 제조
+  - ERP(Enterprise Resource Planning) : 경영자원 통합 시스템으로 재구축 → 생산성 극대화, 경영혁신기법
+  - BI(Business Intelligence) : 의사결정에 활용하는 일련의 프로세스
+  - CRM(Customer Relationship Management) : 고객 중심 자원 극대화
+  - RTE(Real-Time Enterprise) : 새로운 기업경영시스템, 회사 전 부문의 정보를 하나로 통합.
+- 금융
+  - EAI(Enterprise Application Integration) : 정보를 중앙 집중적 통합, 관리, 사용 가능한 인프라
+  - EDW(Enterprise Data Warehouse): BPR, CRM, BSC 같은 다양한 분석 어플리케이션들을 위한 원천
+- 유통
+  - KMS(knowledge Management System) : 지식관리시스템, 기업 경영을 지식 관점에서 접근.
+  - RFID(Radio Frequency, RF) : 주파수를 이용해 ID를 식별하는 시스템 → 전자태그
+
+### 사회기반구조로서의 데이터베이스
+
+#### 1. 개념
+- 1990년대 **사회 각 부문의 정보화**로 DB 구축 활발
+- 부가가치통신망(VAN)으로 정보망 구축
+- 1990년대 후반 지리, 교통 DB 구축
+- 2000년대 고도화 → 일반 가정에서 정보 쉽게 습득
+
+#### 2. 종류
+- EDI(Electronic Data Interchange) : 무역에 필요한 서류들을 **표준화된 양식**으로 전자적 신호로 변경 → 통신망으로 거래처 전송
+- VAN(Value Added Network) : 부가가치통신망, 공중 전기통신사업자의 통신회선으로 독자 네트워크 형성
+- CALS(Commerce At Light Speed) : 전자상거래 구축을 위한 경영통합정보시스템, 제품의 라이프 사이클 데이터 통합자료를 공유, 교환 가능.
+
+#### 3. 분야별 사회기반 구조의 데이터베이스
+- 물류
+  - CVO(Commercial Vehicle Operation System, 화물운송정보)
+  - PORT-MIS(항만운영정보시스템)
+  - KROIS(철도운영정보시스템)
+- 지리/교통
+  - GIS(Geographic Information System, 지리정보시스템)
+  - RS(Remote Sensing, 원격탐사)
+  - GPS(Global Positioning System, 범지구위치결정시스템)
+  - ITS(Intelligent Transport System, 지능형교통시스템)
+  - LBS(Location Based Service, 위치기반서비스)
+  - SIM(Spatial Information Management, 공간정보관리)
+- 의료
+  - PACS(Picture Archiving and Communications System)
+  - U헬스(Ubiquitous - Health)
+- 교육
+  - NEIS(National Education Information System, 교육행정정보시스템)
+
+
+`}];export{n as default};
