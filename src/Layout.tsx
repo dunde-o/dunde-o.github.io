3b1b5e5c925e6ee534df388a3e8d88cfcdc43d73
inkyu.choi <chldlsrb1000@naver.com>
2025-12-23 10:22:01 +0000

    footer 추가

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Footer from "@/components/Footer";
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,12 @@ const Layout = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <Outlet />
+        <div className="min-h-screen flex flex-col">
+          <div className="flex-1">
+            <Outlet />
+          </div>
+          <Footer />
+        </div>
       </TooltipProvider>
     </QueryClientProvider>
   );
